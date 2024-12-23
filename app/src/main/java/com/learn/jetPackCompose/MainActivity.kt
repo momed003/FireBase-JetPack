42b64e8d3ea1b0a44b7ce12fdbacf14f242eaf51
yassimin Momed <89214962+momed003@users.noreply.github.com>
2024-12-23 15:17:53 +0200

    first commit fireBase

---
+++ app/src/main/java/com/learn/jetPackCompose/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.learn.jetPackCompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.learn.jetPackCompose.ui.screen.Home
+import com.learn.jetPackCompose.ui.theme.FireBaseComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+          window.statusBarColor=getColor(R.color.teal_700)
+            Home()
+        }
+    }
+}
